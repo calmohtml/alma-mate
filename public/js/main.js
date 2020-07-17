434784e74563594a93ebc4da2f34c8ccb404e3f6
calmohtml <contactosegovia@protonmail.com>
2020-07-17 13:49:30 -0300

    Detalles de las vistas

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,17 @@
 
 console.log('js');
 
-ScrollReveal().reveal('.hero');
+ScrollReveal().reveal('.hero', {delay: 500 });
 ScrollReveal().reveal('.products', { delay: 500 });
 ScrollReveal().reveal('.banner', { delay: 500 });
-
-console.log('hola me llamo Carlos');
+ScrollReveal().reveal('.container', {delay: 500 });
+ScrollReveal().reveal('.container__register', { delay: 500 });
+ScrollReveal().reveal('.img__register', { delay: 500 });
+ScrollReveal().reveal('.img__register', { delay: 500 });
+ScrollReveal().reveal('.signup__register', { delay: 500 })
+ScrollReveal().reveal('.signup__register--container', { delay: 500 })
+ScrollReveal().reveal('.container__register', { delay: 500 })
+ScrollReveal().reveal('.contactUs', { delay: 500 })
+ScrollReveal().reveal('.container-productAdd', { delay: 500 })
+ScrollReveal().reveal('.productAdd-title', { delay: 500 })
+ScrollReveal().reveal('.form-productAdd', { delay: 500 })
