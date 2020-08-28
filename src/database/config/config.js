4d51a30967b0605f6a62d3b483f364431e236f66
calmohtml <contactosegovia@protonmail.com>
2020-08-28 16:22:19 -0300

    Solucionado el error de product__image--productAdd

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "almamate",
+    "database": "alma-mate",
     "host": "127.0.0.1",
     "dialect": "mysql",
     'define': {
